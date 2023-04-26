4a6aca5ac67efeed414b8b766c326f5858eccbf9
Nileeshp <106258681+Nileeshp@users.noreply.github.com>
2023-04-26 14:16:57 +0530

    postgres db added

--- server.js
+++ server.js
@@ -7,84 +7,95 @@ server.use(bodyParser.json());
 const port = process.env.PORT || 8080;
 
 
-//ESTABLISH DB CONNECTION
-const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password:process.env.DB_PASSWORD || '',
-    database:process.env.DB_NAME || 'AaplMarket'
-})
+const { Client } = require('pg');
+const client = new Client('postgres://aaplmarket_d6ik_user:SdrErISXUtYL4wK6pKIYjLhrxVAm0lq8@dpg-ch4c1764dad97s1v7tng-a/aaplmarket_d6ik')
 
-db.connect(function(error){
-    console.log("db host ", process.env.DB_HOST)
-    if(error){
-        console.log('Error connecting DB ');
+client.connect(function(err) {  
+    if (err){
+        console.log('Error connecting DB ')
     }else{
-        console.log('Successfully connected DB');
+        console.log("Successfully connected DB");
     }
-})
+  });
 
-server.get("/", (req, res) =>{
-    res.send("I m live")
-})
-
-server.get("/service", (req, res) =>{
-    res.send("I m service")
-})
-
-// server.listen(port, function(error){
-//     if(error) console.log("Error... !!!");
-//     else console.log("Started... !!!")
+//ESTABLISH DB CONNECTION
+// const db = mysql.createConnection({
+//     host: process.env.DB_HOST || 'localhost',
+//     user: process.env.DB_USER || 'root',
+//     password:process.env.DB_PASSWORD || '',
+//     database:process.env.DB_NAME || 'AaplMarket'
 // })
 
-server.listen(port, function check(error){
-    if(error){
-        console.log("Error... !!!")
-    }else{
-        console.log("Started... !!! port")
-    }
-});
+// db.connect(function (error) {
+//     console.log("db host ", process.env.DB_HOST)
+//     if (error) {
+//         console.log('Error connecting DB ');
+//     } else {
+//         console.log('Successfully connected DB');
+//     }
+// })
+
+// server.get("/", (req, res) => {
+//     res.send("I m live")
+// })
+
+// server.get("/service", (req, res) => {
+//     res.send("I m service")
+// })
+
+// // server.listen(port, function(error){
+// //     if(error) console.log("Error... !!!");
+// //     else console.log("Started... !!!")
+// // })
+
+// server.listen(port, function check(error) {
+//     if (error) {
+//         console.log("Error... !!!")
+//     } else {
+//         console.log("Started... !!! port")
+//     }
+// });
 
 
-//Insert
-server.post("/api/demo/add", (req, res)=>{
-    let details = {
-        name: req.body.name,
-        course: req.body.course,
-        fee: req.body.fee
-    };
-    let sql = "INSERT INTO demo SET ?";
-    db.query(sql, details, (error)=>{
-        if(error){
-            res.send({status:false, message:'demo created failed'});
-        }else{
-            res.send({status:true, message:'demo created successfully'});
-        }
-    });
-});
+// //Insert
+// server.post("/api/demo/add", (req, res) => {
+//     let details = {
+//         name: req.body.name,
+//         course: req.body.course,
+//         fee: req.body.fee
+//     };
+//     let sql = "INSERT INTO demo SET ?";
+//     db.query(sql, details, (error) => {
+//         if (error) {
+//             res.send({ status: false, message: 'demo created failed' });
+//         } else {
+//             res.send({ status: true, message: 'demo created successfully' });
+//         }
+//     });
+// });
 
-//View
-server.get("/api/demo", (req, res)=>{
-    var sql="SELECT * FROM demo";
-    db.query(sql, function(error, result){
-        if(error){
-            console.log("Error Connectig to DB")
-        }else{
-            res.send({status: true, data: result})
-        }
-    });
-});
+// //View
+// server.get("/api/demo", (req, res) => {
+//     var sql = "SELECT * FROM demo";
+//     db.query(sql, function (error, result) {
+//         if (error) {
+//             console.log("Error Connectig to DB")
+//         } else {
+//             res.send({ status: true, data: result })
+//         }
+//     });
+// });
 
 
-//View by Id
-server.get("/api/demo/:id", (req, res)=>{
-    var sql="SELECT * FROM demo WHERE id=" + req.params.id;
-    db.query(sql, function(error, result){
-        if(error){
-            console.log("Error Connectig to DB")
-        }else{
-            res.send({status: true, data: result})
-        }
-    });
-});
+// //View by Id
+// server.get("/api/demo/:id", (req, res) => {
+//     var sql = "SELECT * FROM demo WHERE id=" + req.params.id;
+//     db.query(sql, function (error, result) {
+//         if (error) {
+//             console.log("Error Connectig to DB")
+//         } else {
+//             res.send({ status: true, data: result })
+//         }
+//     });
+// });
 
